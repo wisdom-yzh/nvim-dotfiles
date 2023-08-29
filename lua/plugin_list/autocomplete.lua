507dd2d55f0da32c9780c792893392c20debcf14
wisdom <wisdom.yinzh@gmail.com>
2023-08-30 00:04:52 +0800

    update tab key mapping for nvim-cmp

--- lua/plugin_list/autocomplete.lua
+++ lua/plugin_list/autocomplete.lua
@@ -1,16 +1,39 @@
 local vim = vim
 local _M = {}
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
 local function cmp_keybindings(cmp)
     return {
         ['<CR>'] = cmp.mapping.confirm({
             select = true ,
             behavior = cmp.ConfirmBehavior.Replace
         }),
-        ['<Tab>'] = cmp.mapping.confirm({
-            select = true ,
-            behavior = cmp.ConfirmBehavior.Replace
-        }),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.confirm({
+                    select = true,
+                })
+            elseif vim.fn["vsnip#available"](1) == 1 then
+                feedkey("<Plug>(vsnip-expand-or-jump)", "")
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+            end
+        end, { "i", "s" }),
+        ['<S-Tab>'] = cmp.mapping(function()
+            if vim.fn["vsnip#jumpable"](-1) == 1 then
+                feedkey("<Plug>(vsnip-jump-prev)", "")
+            end
+        end, { "i", "s"}),
         ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
         ['<Down>'] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), {'i'}),
         ['<Up>'] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), {'i'}),
