b04f86aa2f13f63d65f97e29d379b64c74d7e6f7
wisdom <wisdom.yinzh@gmail.com>
2023-12-20 11:08:09 +0800

    Update rust.lua

--- lua/languages/rust.lua
+++ lua/languages/rust.lua
@@ -32,7 +32,7 @@ _M.lint = function ()
     vim.cmd [[
         let g:ale_rust_cargo_use_clippy = executable('cargo-clippy')
 
-        let b:rust_default_edition = '2018'
+        let b:rust_default_edition = '2021'
         let b:rust_edition = trim(system('cargo get --edition 2>/dev/null'))
         if v:shell_error > 0 || len(b:rust_edition) == 0
             let b:rust_edition = b:rust_default_edition
