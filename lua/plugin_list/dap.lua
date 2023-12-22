1c9e08d90f93abc5e893a2a8912e08c2528e3e10
wisdom <wisdom.yinzh@gmail.com>
2023-12-22 19:18:16 +0800

    Update dap.lua

--- lua/plugin_list/dap.lua
+++ lua/plugin_list/dap.lua
@@ -145,7 +145,7 @@ _M.run = function ()
                         name = "Debug",
                         request = "launch",
                         showLog = false,
-                        program = "./${relativeFileDirname}"
+                        program = "./${relativeFileDirname}",
                         dlvToolPath = vim.fn.exepath("dlv"), -- Adjust to where delve is installed
                     },
                     {
